ccf56a59d92def876eea1a6e1d8056f17b906f02
mwendwamark <mwendwamark310@gmail.com>
2024-05-06 13:20:35 +0300

    Create the navigation bar and make it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React  from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div></div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,68 @@
+import React,{ useState } from "react";
+import { NavLink } from "react-router-dom";
+import { IoMdMenu, IoMdClose } from "react-icons/io"; 
+import { MdOutlinePhone } from "react-icons/md";
+// IMPORT FILES
+import Logo from "../assets/file.png";
+import "../styles/Navbar.css";
+
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container container">
+        <div className="logo-section">
+          <NavLink to="/">
+            <img src={Logo} alt="" />
+          </NavLink>
+        </div>
+
+        <div className={`nav-elements ${showNavbar && "active"}`}>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/service">Service</NavLink>
+            </li>
+            <li>
+              <NavLink to="/appointment">Appointment</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contacts">Contacts</NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <div className="book-appointment-section">
+          <ul>
+            <li>
+              <div className="emergency-call-contents">
+                <MdOutlinePhone className="phone-icon" />{" "}
+                <p>+254 712 345 678</p>
+              </div>
+            </li>
+            <li className="appointment-btn">
+              <NavLink to="/book-appointment">Book Appointment</NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          {showNavbar ? <IoMdClose /> : <IoMdMenu />}{" "}
+          {/* Show close icon when navbar is open */}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
